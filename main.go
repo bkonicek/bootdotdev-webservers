65c22f876f6b6a82c7b2a030b9b04491ed602360
Ben Konicek <ben.konicek@gmail.com>
2024-08-11 15:34:40 -0400

    5-fileserver solution

--- main.go
+++ main.go
@@ -7,8 +7,12 @@ import (
 
 func main() {
 	const port = ":8080"
+	const defaultPath = "."
 
 	mux := http.NewServeMux()
+
+	mux.Handle("/", http.FileServer(http.Dir(defaultPath)))
+
 	server := &http.Server{
 		Handler: mux,
 		Addr:    port,
