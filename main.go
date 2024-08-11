789b0c82c8ab22cbf50ed36d7ae7c8895457aa57
Ben Konicek <ben.konicek@gmail.com>
2024-08-11 18:55:48 -0400

    add stateful metric handling

--- main.go
+++ main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
+
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
+}
+
+func (cfg *apiConfig) metricsResetHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits = 0
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits reset to 0"))
+}
 
 func readinessHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
@@ -15,9 +37,17 @@ func main() {
 	const port = ":8080"
 	const rootPath = "."
 
+	apiMetrics := &apiConfig{}
+
 	mux := http.NewServeMux()
-	mux.Handle("/app/*", http.StripPrefix("/app", http.FileServer(http.Dir(rootPath))))
-	mux.Handle("/healthz", http.HandlerFunc(readinessHandler))
+	mux.Handle("/app/*", http.StripPrefix(
+		"/app", apiMetrics.middlewareMetricsInc(
+			http.FileServer(http.Dir(rootPath)),
+		),
+	))
+	mux.HandleFunc("/healthz", readinessHandler)
+	mux.HandleFunc("/metrics", apiMetrics.metricsHandler)
+	mux.HandleFunc("/reset", apiMetrics.metricsResetHandler)
 
 	server := &http.Server{
 		Handler: mux,
